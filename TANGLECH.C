e8cd5a0e17ffdfc950452bc4148982b36a4bf3bb
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2022-11-11 20:07:56 +0530

    Add files via upload

---
+++ TANGLECH.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+int angle1=0,angle2=0,angle3=0;
+int isoclerales=0,scalence=0;
+printf("Enter The angle1");
+scanf("%d",&angle1);
+printf("Enter The angle2");
+scanf("%d",&angle2);
+printf("Enter The angle3");
+scanf("%d",&angle3);
+	if (angle1== angle2 && angle3==angle2)
+		printf("Equlateral trangle");
+
+getch();
+}

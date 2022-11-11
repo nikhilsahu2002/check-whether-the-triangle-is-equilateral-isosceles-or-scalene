442b36a1d9f0109260afa19b0c836032a929190c
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2022-11-11 20:09:14 +0530

    Update TANGLECH.C

--- TANGLECH.C
+++ TANGLECH.C
@@ -3,7 +3,6 @@
 void main ()
 {
 int angle1=0,angle2=0,angle3=0;
-int isoclerales=0,scalence=0;
 printf("Enter The angle1");
 scanf("%d",&angle1);
 printf("Enter The angle2");
@@ -12,6 +11,11 @@ printf("Enter The angle3");
 scanf("%d",&angle3);
 	if (angle1== angle2 && angle3==angle2)
 		printf("Equlateral trangle");
-
+	else if (angle1==angle2 || angle2==angle3 || angle3==angle1)
+		printf("isocleation trangle");
+	else
+		pirntf("scaler trangle");
 getch();
 }
+
+
